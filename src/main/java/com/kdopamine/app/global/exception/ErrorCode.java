c30ca26c5e46bdd3e6c9dd29dc0d29f19530a2c1
aneunwook <dmsdnr0720@naver.com>
2026-01-07 13:36:16 +0900

    refactor : 에러코드 수정

--- src/main/java/com/kdopamine/app/global/exception/ErrorCode.java
+++ src/main/java/com/kdopamine/app/global/exception/ErrorCode.java
@@ -16,19 +16,20 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(404, "A007", "해당 사용자를 찾을 수 없습니다."),
 
     // Common
-    INVALID_INPUT_VALUE(400, "C001", "잘못된 입력값입니다."),
-    METHOD_NOT_ALLOWED(405, "C002", "허용되지 않은 메서드입니다."),
-    INTERNAL_SERVER_ERROR(500, "C003", "서버 오류가 발생했습니다."),
-    INVALID_TYPE_VALUE(400, "C004", "잘못된 타입입니다."),
+    INVALID_INPUT_VALUE(400, "B001", "잘못된 입력값입니다."),
+    METHOD_NOT_ALLOWED(405, "B002", "허용되지 않은 메서드입니다."),
+    INTERNAL_SERVER_ERROR(500, "B003", "서버 오류가 발생했습니다."),
+    INVALID_TYPE_VALUE(400, "B004", "잘못된 타입입니다."),
 
     // Business
-    RESOURCE_NOT_FOUND(404, "B001", "리소스를 찾을 수 없습니다."),
-    DUPLICATE_RESOURCE(409, "B002", "중복된 리소스입니다."),
-    UNAUTHORIZED(401, "B003", "인증이 필요합니다."),
-    FORBIDDEN(403, "B004", "권한이 없습니다."),
+    RESOURCE_NOT_FOUND(404, "C001", "리소스를 찾을 수 없습니다."),
+    DUPLICATE_RESOURCE(409, "C002", "중복된 리소스입니다."),
+    UNAUTHORIZED(401, "C003", "인증이 필요합니다."),
+    FORBIDDEN(403, "C004", "권한이 없습니다."),
 
-    //category
-    CATEGORY_NOT_FOUND(404, "C001","해당 카테고리가 없습니다.");
+    //words
+    CATEGORY_NOT_FOUND(404, "D001","해당 카테고리가 없습니다."),
+    WORD_NOT_FOUND(404, "D002", "해당 단어가 없습니다.");
 
     private final int status;
     private final String code;
